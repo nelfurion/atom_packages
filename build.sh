1958ede3c5dc60745a476e180626daad23bb35f4
nelfurion <angelovradostin@gmail.com>
2020-01-12 12:53:36 +0000

    adding the script for package installation in build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
-if !(cat ~/.bashrc | grep -c "alias atom") then
+if !(cat ~/.bashrc | grep -c "alias atom" -q) then
   echo "\n" >> ~/.bashrc
   echo "# Setting a proxy for the atom command in order to use profiles" >> ~/.bashrc
   echo "alias atom=\"$(pwd)/atom.sh\"" >> ~/.bashrc
-  
+
   # Activate the atom proxy for the current shell process
   alias atom="$(pwd)/atom.sh"
 
   # Make it executable without the need to sudo
-  chmod a+x ($pwd)/atom.sh
+  chmod a+x "$(pwd)/atom.sh"
 fi
+
+echo "Setting atom proxy done!"
+sh ./install-packages.sh
